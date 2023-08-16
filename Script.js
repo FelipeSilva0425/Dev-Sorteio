5fa0e1ed54357ff855bfa4b58fa1a662189cdfc8
felipesilva0425 <felipesilva061991@gmail.com>
2023-08-16 01:56:51 -0300

    projeto finalizado

---
+++ Script.js
@@ -0,0 +1,14 @@
+function generateNumber(){
+    const min = Math.ceil( document.querySelector(".inputMin").value)
+    const max = Math.floor( document.querySelector(".inputMax").value)
+    const mensage = document.querySelector(".message")
+    const inputResult = document.querySelector(".input-result")
+
+        if(min >= max){
+            alert("Primeiro valor precisa ser menor que o segundo valor!")
+        }else{
+    const resut = Math.floor(Math.random()*(max-min+1))+min;
+    mensage.innerHTML = `O número sorteado é:`
+    inputResult.innerHTML = resut
+            }
+}
